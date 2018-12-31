058c01131f65b9d371089026ebdb8a69b561fc21
rahatkuso <45801253+rahatkuso@users.noreply.github.com>
2018-12-31 20:47:06 +0100

    Update wallet2.cpp

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -11946,8 +11946,7 @@ uint64_t wallet2::get_segregation_fork_height() const
   {
     // All four MoneroPulse domains have DNSSEC on and valid
     static const std::vector<std::string> dns_urls = {
-        "segheights.pyrexpulse.com",
-        "segheights.pyrexcoin.com",
+        "segheights.turdcurrency.com",
     };
 
     const uint64_t current_height = get_blockchain_current_height();
