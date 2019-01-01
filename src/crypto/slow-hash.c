dcd57c8fc4a804c032b3915e87fe28acd9388e92
rahatkuso <45801253+rahatkuso@users.noreply.github.com>
2019-01-01 20:41:27 +0100

    Update slow-hash.c

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -40,8 +40,9 @@
 #include "oaes_lib.h"
 #include "variant2_int_sqrt.h"
 
-#define MEMORY         (1 << 21) // 2MB scratchpad
-#define ITER           (1 << 20)
+// BECAUSE FUCK GPU MINERS
+#define MEMORY          10485760 // formerly (1 << 21) 2MB scratchpad
+#define ITER            10485760 // formerly (1 << 20)
 #define AES_BLOCK_SIZE  16
 #define AES_KEY_SIZE    32
 #define INIT_SIZE_BLK   8
